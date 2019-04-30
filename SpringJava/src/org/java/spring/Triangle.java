39138c5d26098d500fcad64655f8f07146569b1e
arun26cs <arun26cs@gmail.com>
2019-04-29 23:44:03 -0400

    object injection using ref key word in spring.xml file

--- SpringJava/src/org/java/spring/Triangle.java
+++ SpringJava/src/org/java/spring/Triangle.java
@@ -2,45 +2,37 @@ package org.java.spring;
 
 public class Triangle{
 
-	private Triangle Triangle;
-	private char type;
-	private int height;
-	public Triangle(char type) {
-		this.type=type;
-	}
-	
-	public Triangle(int height) {
-		this.height=height;
-	}
-	
-	
-	public Triangle(char type,int height) {
-		this.type=type;
-		this.height=height;
-	}
-	
-	
-	public int getHeight() {
-		return height;
+	Point point1;
+	Point point2;
+	Point point3;
+	//from xml the value falls to set() and then returned by get()
+	public Point getPoint1() {
+		return point1;
 	}
 
-	public char getType() {
-		return type;
+	public void setPoint1(Point point1) {
+		this.point1 = point1;
 	}
 
-//	public void setType(char type) {
-//		this.type = type;
-//	}
-
-//	public void setTriangle(Triangle Triangle) {
-//		this.Triangle=Triangle;
-//	}
-	
-	public Triangle getTriangle() {
-		return this.Triangle;
+	public Point getPoint2() {
+		return point2;
 	}
-	
+
+	public void setPoint2(Point point2) {
+		this.point2 = point2;
+	}
+
+	public Point getPoint3() {
+		return point3;
+	}
+
+	public void setPoint3(Point point3) {
+		this.point3 = point3;
+	}
+
 	public void draw() {
-		System.out.println(" Drwing "+getType()+" triangle having height "+ getHeight());
+		System.out.println("Drwing triangle having point "+ getPoint1().getX()+" and "+ getPoint1().getY());
+		System.out.println("Drwing triangle having point "+ getPoint2().getX()+" and "+ getPoint2().getY());
+		System.out.println("Drwing triangle having point "+ getPoint3().getX()+" and "+ getPoint3().getY());
 	}
 }
